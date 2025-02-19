ef0627fc9ee228d5ac2fb77dbef786d50b3c17ed
Klotz <julius.klotz01@sap.com>
2025-02-19 15:35:19 +0100

    Update

--- fish/config.fish
+++
@@ -1,9 +0,0 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
-set -U fish_greeting
-
-alias odsmock="cd ~/personal_workspace/order-delivery-schedule/app && npm run serve:mock"
-alias odsdock="cd ~/personal_workspace/order-delivery-schedule && docker compose down && docker compose up --build"
-alias odsbuild="cd ~/personal_workspace/order-delivery-schedule && mvn clean package -DskipTests"
